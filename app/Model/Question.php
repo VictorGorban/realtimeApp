505c91ab4c914faabeb0e2f982adc61ad1e03cfe
VictorGorban <victorgorban2@ya.ru>
2019-07-15 19:48:24 +0300

    added relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,39 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+
 
 class Question extends Model
 {
-    //
+
+    /*
+     * in fact, we describe the model here. But these methods
+     * can be used to retrieve linked records from DB.
+     * */
+
+    /**
+     * @return user that we linked (many to one)
+     */
+    function user(){
+        /*many to one or inversed one-to-one/one-to-many*/
+        return  $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return many replies that linked us (one to many)
+     */
+    function replies(){
+        /*one to many*/
+        return $this->hasMany(Reply::class);
+
+    }
+
+    function category(){
+        /*many to one*/
+        return $this->belongsTo(Category::class);
+
+    }
+
+
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -2,9 +2,24 @@
 
 namespace App\Model;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
     //
+    function question(){
+        /*many-to-one, reversed one-to-many*/
+        return $this->belongsTo(Question::class);
+    }
+
+    function user(){
+        /*many-to-one, reversed one-to-many*/
+        return $this->belongsTo(User::class);
+    }
+
+    function likes(){
+        /*one-to-many*/
+        return $this->hasMany(Like::class);
+    }
 }
