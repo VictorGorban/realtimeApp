370bb2c1577bc0b084df90e32a17d40aa32c647b
VictorGorban <victorgorban2@ya.ru>
2019-07-16 15:12:12 +0300

    Factories made. Next step is finally seeding.

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,12 +1,17 @@
 <?php
 
-/* @var $factory \Illuminate\Database\Eloquent\Factory */
+/* @var $factory Factory */
 
 use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $name = $faker->sentence;
     return [
-        //
+        'name' => $name,
+        'slug' => Str::slug($name),
+
     ];
 });
